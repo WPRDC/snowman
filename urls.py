a72a5ec29f99008088de4d00d4fdd7061ee7187b
saylorsd <steven.saylor@pitt.edu>
2018-02-12 14:55:00 -0500

    allow for no trailing slash

--- urls.py
+++ urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^records/$', views.snow_plow_records, name='snow_plow_records'),
+    url(r'^records$', views.snow_plow_records, name='snow_plow_records'),
 ]
